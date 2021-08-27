f9a67d326c96dad048314212f330f27eca5202eb
VDA-001 <vdavishu@gmail.com>
2021-08-27 23:50:24 +0530

    Update :fire: Completed the user registration part

--- api/user/models.py
+++ api/user/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class CustomUser(AbstractUser):
-    username = models.CharField(max_length=50)
+class CustomUser(models.Model):
+    name = models.CharField(max_length=100,default="Default")
     email = models.EmailField(max_length=250 , unique=True)   
+
+    def __str__(self):
+        return self.name
--- api/user/views.py
+++ api/user/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from .models import CustomUser
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
+def register(request):
+    if not request.method == 'GET':
+        return JsonResponse({'error':'Send only get request'})
+    try:
+        username = request.GET['username']
+        email = request.GET['email']
+    except:
+        return JsonResponse({"error":"Not found username or email field"})
+
+    if CustomUser.objects.filter(email=email):
+        return JsonResponse({"error":"Email already exists"})
+    else:
+        instance = CustomUser.objects.create(email=email,name=username)
+        instance.save();
+        return JsonResponse({"Success":"Successfully registered the user"});
+        
